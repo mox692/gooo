02e53a1e1e551ccfdf6147f3f29ac106b3349047
mox692 <55653825+mox692@users.noreply.github.com>
2021-08-02 23:15:07 +0900

    feat: gobin

---
+++ gobin/cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/mox692/gooo/gobin"
+)
+
+var fileName = "./cmd/main"
+
+func main() {
+	f, err := os.Open(fileName)
+	if err != nil {
+		fmt.Printf("%s", err.Error())
+		return
+	}
+
+	lib, err := gobin.Parse(f)
+	if err != nil {
+		fmt.Printf("%s", err.Error())
+		return
+	}
+
+	for _, v := range lib {
+		fmt.Printf("Name: %s, Version: %s", v.Name, v.Version)
+	}
+}
---
+++ gobin/go.mod
@@ -0,0 +1,5 @@
+module github.com/mox692/gooo/gobin
+
+go 1.15
+
+require github.com/aquasecurity/go-dep-parser v0.0.0-20210802113332-f09967d68412
